e0955ed2fd0985ceac511934a399aa50a7e5d75f
arjun <arjun.94.choudhry@gmail.com>
2017-12-23 11:18:56 -0600

    fitting and predicting the model

--- svm/svm.py
+++ svm/svm.py
@@ -13,3 +13,35 @@ X = sc_X.fit_transform(X)
 
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.25,random_state=0)
+
+# fitting the svm model
+from sklearn.svm import SVC
+# Since, we are starting with modelling the basic svc model, we will start with the linear kernel. This will make our classifier look like the linear or logistic classifier.
+svmClassifier = SVC(kernel='linear', random_state= 0)
+svmClassifier.fit(X_train, y_train)
+
+# Predicting using the fitted model
+y_pred = svmClassifier.predict(X_test)
+
+# Testing validity of model using confusion matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test,y_pred)
+
+# Plotting the graph
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+
+plt.contourf(X1, X2, svmClassifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('SVM (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()

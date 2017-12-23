711e1f2565d3b7b5e79b8b80d8a30a44023c2578
arjun <arjun.94.choudhry@gmail.com>
2017-12-23 04:03:37 -0600

    adding the confusion matrix

--- logistic_regression/logistic_regression.py
+++ logistic_regression/logistic_regression.py
@@ -39,3 +39,7 @@ classifier.fit(X_train,y_train)
 
 # Predicting the model on the test set
 y_pred = classifier.predict(X_test)
+
+# testing the validity of model using confusion matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test,y_pred)

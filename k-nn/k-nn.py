911263a8504e11f697d135f645fd2c76866310e4
arjun <arjun.94.choudhry@gmail.com>
2017-12-23 06:22:20 -0600

    fitting the model

--- k-nn/k-nn.py
+++ k-nn/k-nn.py
@@ -21,3 +21,16 @@ X = sc_X.fit_transform(X)
 
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 0.2, random_state=0)
+
+# fitting the K-NN classification model
+from sklearn.neighbors import KNeighborsClassifier
+# metric = 'minkowski', p =2 are attributes required to instruct the model to follow Euclidean matrix
+classifier = KNeighborsClassifier(n_neighbors = 5, metric = 'minkowski', p =2)
+classifier.fit(X_train,y_train)
+
+# Predicting the values
+y_pred = classifier.predict(X_test)
+
+# Testing validity with confusion matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test,y_pred)

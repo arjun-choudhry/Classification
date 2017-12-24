9c020d269dc2c6d7403640eb0bf836bdfa169300
arjun <arjun.94.choudhry@gmail.com>
2017-12-24 06:29:56 -0600

    data_preprocessing

---
+++ kernel_svm/kernel_svm.py
@@ -0,0 +1,21 @@
+"""
+@author: arjun
+"""
+# *********************************************************************************************************************
+# AIM: Create a classification model to predict the purchasing activity of a group based on the given data using kernel SVM
+# *********************************************************************************************************************
+# Data Preprocessing
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:,[2,3]].values
+y = dataset.iloc[:,4].values
+
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X = sc_X.fit_transform(X)
+
+from sklearn.cross_validation import train_test_split
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.25,random_state=0)
